f9840526e7ebb04139d3e5153cef004ecf29adc2
lulinieva <evelynieva@hotmail.com>
2021-04-12 16:22:06 -0300

    Agregamos el uso del operador" instanceof que nos permite checkeaar si una variable apunta a un objeto de X tipo. Al mismo tiempo usamos el operado de "CASTEO"(Tipo) para hacer que una variable sea representable por un tipo diferente.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,9 @@
+
+import java.util.*;
+
 public class App {
+
+    public static Scanner Teclado = new Scanner(System.in); 
     public static void main(String[] args) throws Exception {
         //A partir de METFILX buscar la temporada 5 y el episodio 1 de la serie
         //How I Met Your Mother y
@@ -11,18 +16,47 @@ public class App {
         miMetflix.inicializarCatalogo(); //Estoy llamando al metodo que crea ls pelis y series.
 
         Serie serieBuscada = miMetflix.buscarSerie("How I Met Your Mother");
+
         if (serieBuscada == null){
-            System.out.println("Serie ni existe");
+
+            System.out.println("No se encontro la serie");
             return;
         }
+        System.out.println("Ingres nro temporada ");
+        
+        int nroTemporada = Teclado.nextInt();
+        Teclado.nextLine();
 
-        Temporada temporada = serieBuscada.buscarTemporada(5);
+        Temporada temporada = serieBuscada.buscarTemporada(nroTemporada);
        //buscar y guardar una VARIABLE
-       Episodio episodio = temporada.buscarEpisodio(1);
+       System.out.println("Ingrese nr episodio ");
+       int nroEpisodio = Teclado.nextInt();
+       Teclado.nextLine();
+
+       Episodio episodio = temporada.buscarEpisodio(nroEpisodio);
 
        episodio.reproducir(); 
 
-       public Serie reproducirSerie(String nombreSerie, int)
+       //ahora quiero imprimir un cartel que diga
+       //que lo que se acaba de reproducir arriba es un
+       //websodio o un episodio
+       //pero en un print aparte.
+       
+       if(episodio instanceof Websodio){
+           System.out.println("El episodio era un websodio ");
+
+       }
+       else {
+           System.out.println("Es un episodio");
+       }
+
+       if (episodio instanceof Websodio){
+           //Castear
+           Websodio websodio = (Websodio)episodio; 
+           System.out.println("El link del websodio es: "+ websodio.link);
+       }
+
+
 
       
 
